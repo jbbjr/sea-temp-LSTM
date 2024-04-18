74f587fe0e066c7a57f4d160c7f97c7dfb588f4c
Bennett Blanco <109486714+jbblancojr@users.noreply.github.com>
2024-04-17 20:44:30 -0400

    adding daily_avg query

---
+++ daily_averages.sql
@@ -0,0 +1,38 @@
+WITH DailyAverages AS (
+    SELECT 
+        year, month, day, latitude, longitude,
+        AVG(wind_direction_true) AS avg_wind_direction_true,
+        AVG(wind_speed) AS avg_wind_speed,
+        AVG(visibility) AS avg_visibility,
+        AVG(present_weather) AS avg_present_weather,
+        AVG(past_weather) AS avg_past_weather,
+        AVG(sea_level_pressure) AS avg_sea_level_pressure,
+        AVG(air_temperature) AS avg_air_temperature,
+        AVG(wetbulb_temperature) AS avg_wetbulb_temperature,
+        AVG(dewpoint_temperature) AS avg_dewpoint_temperature,
+        AVG(total_cloud_amount) AS avg_total_cloud_amount,
+        AVG(sea_surface_temp) AS avg_sea_surface_temp
+    FROM 
+        `bigquery-public-data.noaa_icoads.icoads_core_*`
+    WHERE
+        _TABLE_SUFFIX BETWEEN '2005' AND '2015'
+    GROUP BY 
+        year, month, day, latitude, longitude
+)
+SELECT 
+    year, month, day, latitude, longitude,
+    avg_wind_direction_true,
+    avg_wind_speed,
+    avg_visibility,
+    avg_present_weather,
+    avg_past_weather,
+    avg_sea_level_pressure,
+    avg_air_temperature,
+    avg_wetbulb_temperature,
+    avg_dewpoint_temperature,
+    avg_total_cloud_amount,
+    avg_sea_surface_temp
+FROM 
+    DailyAverages
+WHERE
+    avg_sea_surface_temp IS NOT NULL
